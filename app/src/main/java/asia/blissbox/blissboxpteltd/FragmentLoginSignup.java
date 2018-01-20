efa94bb635c690836d1c2f8680218875b764a1fd
ItsYaBoiBry <bryanlow987@gmail.com>
2018-01-20 17:45:15 +0800

    Facebook Login success. Google Login Error
    TODO(Login Api for blissbox)

--- app/src/main/java/asia/blissbox/blissboxpteltd/FragmentLoginSignup.java
+++ app/src/main/java/asia/blissbox/blissboxpteltd/FragmentLoginSignup.java
@@ -14,9 +14,16 @@ import android.widget.Button;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.SignInButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class FragmentLoginSignup extends Fragment {
     TextInputEditText email, password;
@@ -45,12 +52,35 @@ public class FragmentLoginSignup extends Fragment {
         fbLogin = view.findViewById(R.id.fbLogin);
         login = view.findViewById(R.id.blissboxLogin);
         gLogin = view.findViewById(R.id.gLogin);
-        fbLogin.setReadPermissions("email");
+        fbLogin.setReadPermissions(Arrays.asList("public_profile", "email"));
         fbLogin.setFragment(this);
         fbLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.e("result", String.valueOf(loginResult.getAccessToken()));
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.wtf("LoginActivity", response.toString());
+
+
+                                // Application code
+                                try {
+                                    String email = object.getString("email");
+                                    String birthday = object.getString("birthday"); // 01/31/1980 format
+                                    Log.e("email", email + "");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
 
             }
 
